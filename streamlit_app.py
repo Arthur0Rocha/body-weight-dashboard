bc35ce8ebef56d19701d831b355ee37863ff3e95
Arthur0Rocha <ar_d_rocha@hotmail.com>
2024-06-02 00:58:34 -0300

    added plot function to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-st.title("Hello, World!")  # Display a title
-st.write("This is a Streamlit app.")  # Display some text
+from src.body import plot_body_weight 
+
+st.title("Weight data.")  # Display a title
+st.write("This is a line plot of weight measures.")  # Display some text
+
+st.pyplot(plot_body_weight())
 
 # You can add more elements here, like:
 # - Images: st.image("path/to/image.jpg")
