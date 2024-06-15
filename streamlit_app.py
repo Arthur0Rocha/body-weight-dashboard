dd37cb9870a6a9cc75d1ff30475701e6443858c9
Arthur Rocha <arthur.rocha@serpro.gov.br>
2024-06-14 22:07:02 -0300

    Streamlit hello world

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
-print("Dale")
+import streamlit as st
+
+st.title("Hello, World!")  # Display a title
+st.write("This is a Streamlit app.")  # Display some text
+
+# You can add more elements here, like:
+# - Images: st.image("path/to/image.jpg")
+# - Buttons: st.button("Click me!")
+# - Checkboxes: st.checkbox("Select this")
+# - Text input: name = st.text_input("Enter your name:")
+
